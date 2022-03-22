9a93a93d1f3d16bc4ec6ca6d77bad65939634652
Victor Longo <victor.m.longo@gmail.com>
2022-03-22 10:54:27 -0300

    eslint desactivado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   reactStrictMode: true,
 
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   env: {
     'MYSQL_HOST': '192.168.1.102',
     'MYSQL_PORT': '3306',
